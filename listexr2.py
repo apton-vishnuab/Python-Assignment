924eefc373e471f903b46eef3d1b3bfbea56c47d
apton-anandhuor <anandhu.or@aptonworks@gmail.com>
2022-09-22 11:50:27 +0530

    Added new practice files

---
+++ listexr2.py
@@ -0,0 +1,39 @@
+def remove_adjacent(nums):
+  i = 1
+  while i < len(nums):    
+    if nums[i] == nums[i-1]:
+      nums.pop(i)
+      i -= 1  
+    i += 1
+  return nums
+
+def linear_merge(list1, list2):
+  list1.extend(list2)
+  list1.sort()
+  return list1
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+def main():
+  print('remove_adjacent')
+  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+  test(remove_adjacent([]), [])
+
+ 
+  print('linear_merge')
+  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+       ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+
+if __name__ == '__main__':
+  main()
