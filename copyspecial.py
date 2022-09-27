4658a2a581ee58def3d09db535cd1d94fa45e0de
apton-anandhuor <anandhu.or@aptonworks@gmail.com>
2022-09-27 11:42:42 +0530

    Added new functions

--- copyspecial.py
+++ copyspecial.py
@@ -2,53 +2,65 @@ import sys
 import re
 import os
 import shutil
-from pathlib import Path
-from unittest.mock import patch
+from zipfile import ZipFile
+from os.path import basename
 
+ 
 def get_special_paths(dir):
-    file_content = []
-    ln = len(dir)
-    
-    for i in range(ln):
-      path = os.listdir(dir[i])
-      file_content.extend(path)
+  result = []
+  paths = os.listdir(dir)  
+  for i in paths:
+    match = re.search(r'__(\w+)__', i)
+    if match:
+      result.append(os.path.abspath(os.path.join(dir, i)))
+  return result
+ 
+ 
+def copy_to(paths, to_dir):
+  for i in paths:
+    f = os.path.basename(i)
+    shutil.copy(i, os.path.join(to_dir, f))
+ 
+ 
+def zip_to(dirName):
+  with ZipFile('Generated_zipfile.zip', 'w') as zipObj:
+   for folderName, subfolders, filenames in os.walk(dirName):
+       for filename in filenames:
+           filePath = os.path.join(folderName, filename)
+           zipObj.write(filePath, basename(filePath))
 
-    str_match = [s for s in file_content if "__" in s]
-    print("Folders having Special files are\n")
-    for i in range(0, len(str_match)):
-          print(os.path.abspath(str_match[i]))
-
-def copy_to(paths,to_dir):
-  for path in paths:
-    f = os.path.basename(path)
-    shutil.copy(path, os.path.join(to_dir, f))
+  print("Zip File Generated")
 
 def main():
   args = sys.argv[1:]
   if not args:
-    print ("usage: [--todir dir][--tozip zipfile] dir [dir ...]");
+    print ("usage: [--todir dir][--tozip zipfile] dir [dir ...]")
     sys.exit(1)
 
   todir = ''
   if args[0] == '--todir':
     todir = args[1]
     del args[0:2]
-
+ 
   tozip = ''
   if args[0] == '--tozip':
     tozip = args[1]
     del args[0:2]
-
+ 
   if len(args) == 0:
     print ("error: must specify one or more dirs")
     sys.exit(1)
 
-  get_special_paths(args)
-  if todir:
-    copy_to(paths, dir)
-
+  paths = []
+  for dir in args:
+    paths.extend(get_special_paths(dir))
  
+  if todir:
+    copy_to(paths, todir)
+  elif tozip:
+    zip_to(tozip)
+  else:
+    print ('\n'.join(paths))
+
 if __name__ == "__main__":
   main()
-
-
