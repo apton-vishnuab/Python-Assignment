2df42e13f2f0f8656be3d01706ef57cf1bd13e74
apton-anandhuor <anandhu.or@aptonworks@gmail.com>
2022-09-28 18:19:02 +0530

    Minor Changes

--- LogEdit.py
+++ LogEdit.py
@@ -1,7 +1,5 @@
-import functools
 import os
 import re
-import sys
 import urllib.request, urllib.parse, urllib.error
 import time
 import sys
@@ -11,14 +9,13 @@ def read_urls(filename):
     ub = filename.index('_') # Getting the index of '_' in the filename that given as args.
     host = filename[ub + 1:] # Getting the host name followed after the '_' .
     url_dict = {}
-    f = open(filename)
-    for i in f:
-        match = re.search(r'"GET (\S+)', i) # Regex operation on Log file for finding the rest of URL.
-        if match:
-            path = match.group(1) # The first parenthesized subgroup.
-            if 'puzzle' in path:
-                url_dict['http://' + host + path] = 1
-    
+    with open(filename) as f:
+        for i in f:
+            match = re.search(r'"GET (\S+)', i) # Regex operation on Log file for finding the rest of URL.
+            if match:
+                path = match.group(1) # The first parenthesized subgroup.
+                if 'puzzle' in path:
+                    url_dict['http://' + host + path] = 1
     
     def sort(url):
         match = re.search(r'-(\w+)-(\w+)\.\w+', url)
@@ -30,30 +27,27 @@ def read_urls(filename):
     return sorted(list(url_dict.keys()), key=sort)
 
 def download_images(img_urls, dest_dir):
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
     animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
-    index = open(os.path.join(dest_dir, 'index.html'), 'w')
-    index.write('<html><body>\n')
+    file = open(os.path.join(dest_dir, 'index.html'), 'w')
+    file.write('<html><body>\n')
     i = 0
-    print("Getting Url's, Please wait...")
+    print("Retrieving Url's, Please wait...")
     
-    for img_url in img_urls:
+    for url in img_urls:
         local_name = 'img%d' % i
         time.sleep(0.2)
         sys.stdout.write("\r" + animation[i % len(animation)])
         sys.stdout.flush()
-        urllib.request.urlretrieve(img_url, os.path.join(dest_dir, local_name))
-        index.write('<img src="%s">' % (local_name,))
+        urllib.request.urlretrieve(url, os.path.join(dest_dir, local_name))
+        file.write('<img src="%s">' % (local_name,))
         i += 1
     
     print("\n")
-    index.write('\n</body></html>\n')
-    index.close()
+    file.write('\n</body></html>\n')
+    file.close()
     print("Opening image in browser..")
-    filename = "/home/anandhuor/Documents/PyExercise/Log_Files/index.html"
-    webbrowser.open('file://' + os.path.realpath(filename))
-    exit(0)
+    filename = "/home/anandhuor/Documents/PyExercise/Log_Files/index.html" # Location of generated "index.html" file.
+    webbrowser.open('file://' + os.path.realpath(filename)) # Opening file in browser
 
 def main():
     args = sys.argv[1:]
